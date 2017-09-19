7a932f34da19617a1ed19a614ca60323a136490c
Jesse Bryan and Lois Choi <jesse+lois@>
2017-09-19 11:19:15 -0700

    Add ngIf and two way binding

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule} from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule ({
-  imports: [ BrowserModule ],
+  imports: [ BrowserModule, FormsModule ],
   declarations: [ AppComponent ],
   bootstrap: [ AppComponent ]
   //boostrap refers to launchin an application with min required resources (NOT bootstrap stylesheet)
